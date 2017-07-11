3b9a949d5cfc81ea33fae834c7d9bcedda2c7bd6
Michal Idzik <midzik826@gmail.com>
2017-07-11 19:24:14 +0000

    hello

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+ $('ul li').click(function(){
+    // This function will be called every time one of the tabs is clicked
     
+    var clicked_tab = $(this);
+    
+    // TODO Complete the next line to add the active class to the clicked tab: 
+    clicked_tab.addClass('active');
+    
+    // TODO Complete the next line to remove the active class from the rest:
+    clicked_tab.parent().children('li').not(clicked_tab).removeClass('active');
+    var clicked_href=$(this).children('a').attr('href');
+    $('img').hide();
+    $(clicked_href).show();
+    
+});   
     
     
     
