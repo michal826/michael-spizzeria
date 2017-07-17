272553678ba24c8fc4a29364eba826bb105d92e3
Michal Idzik <midzik826@gmail.com>
2017-07-17 19:03:37 +0000

    hello

--- script.js
+++ script.js
@@ -13,7 +13,35 @@ $(document).ready(function(){
     $('img').hide();
     $(clicked_href).show();
     
-});   
+});  
+<p>Click the button to get your coordinates.</p>
+
+<button onclick="getLocation()">Try It</button>
+
+<p id="demo"></p>
+
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
+
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(40.7298411, -73.99347329999999), zoom: 20
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
     
     
     
