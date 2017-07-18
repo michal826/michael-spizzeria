faf5e01aac7ac0e06ea84424566ce5555b0149d8
Michal Idzik <midzik826@gmail.com>
2017-07-18 19:18:01 +0000

    hello

--- script.js
+++ script.js
@@ -12,46 +12,54 @@ $(document).ready(function(){
     var clicked_href=$(this).children('a').attr('href');
     $('img').hide();
     $(clicked_href).show();
-    
+    $('.message a').click(function(){
+   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
 });  
-<p>Click the button to get your coordinates.</p>
 
-<button onclick="getLocation()">Try It</button>
 
-<p id="demo"></p>
-
-var x = document.getElementById("demo");
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-
-function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;
-}
 
 function myMap() {
   var mapCanvas = document.getElementById("map");
   var mapOptions = {
-    center: new google.maps.LatLng(40.7298411, -73.99347329999999), zoom: 20
+    center: new google.maps.LatLng(40.7306, 74.0021), zoom: 20
   };
   var map = new google.maps.Map(mapCanvas, mapOptions);
 }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 });
+
+function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+
+
+var directionsService = new google.maps.DirectionsService;
+
+
